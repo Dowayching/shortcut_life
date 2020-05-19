a01485ee0ba752d0ebe8f552028cc62ff821d9f8
Dowayching <dowayching@yahoo.com.tw>
2020-05-19 20:03:31 +0800

    [mac alias] add notion alias script

---
+++ mac_alias/notion_alias.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Notion Alias (for Mac)
+# Install: bash this_script
+# Usage: type command, 
+
+# Echo Function Descriptions
+echo "" >> ~/.bash_profile
+echo "# Notion Alias" >> ~/.bash_profile
+
+# Notion page
+echo "alias nt_task=\"open -a /Applications/Notion.app https://www.notion.so/antotu/Task-Focus-dc689865e9b54c43bc20f3799e71b11a\"" >> ~/.bash_profile
+echo "alias nt_project=\"open -a /Applications/Notion.app https://www.notion.so/antotu/Project-Focus-843d7cbfb5444ae4bc01b407112e3cb7\"" >> ~/.bash_profile
