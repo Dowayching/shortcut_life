fc6ab456aef92a52b6ed355f991dd733e31301a2
Dowayching <dowayching@yahoo.com.tw>
2020-05-20 16:59:14 +0800

    [install] update install all scripts function

--- install_alias.sh
+++ install_alias.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-#TODO: Unfinished 
+# Install All Mac Shortcut
+# Usage: bash this_script
+# Comment: clear the duplicated code on ~/.zshrc & ~/.bash_profile before installation
 
-# Install All Mac Alias
-# Install: bash this_script 
+echo "Install All Mac Shortcut:"
 
-
-cd mac_alias
-
-#TODO: make install all files on mac_alias folder 
-
-
-
+# install all scripts on alias folder
+cd alias
+for script in ./*
+do
+	bash $script
+done
 cd ..
 
 # Make the change work
 . ~/.bash_profile
 
-# Write to ZSH setting
-echo "" >> ~/.zshrc
-echo "# Load bash_profile" >> ~/.zshrc
-echo "if [ -f ~/.bash_profile ]; then" >> ~/.zshrc
-echo "  . ~/.bash_profile" >> ~/.zshrc
-echo "fi" >> ~/.zshrc
 
+# Write to ZSH setting to run .bash_profile on 
+# session start
+exec 3>&1 1>> ~/.zshrc
+echo "" 
+echo "# Load bash_profile" 
+echo "if [ -f ~/.bash_profile ]; then" 
+echo "  . ~/.bash_profile" 
+echo "fi" 
+exec 1>&3 3>&-
+
+
+echo "All Mac Shortcuts installed!"
