1549941101f4852744e3f6a0aba43ceed0f977bc
Dowayching <dowayching@yahoo.com.tw>
2020-05-20 10:13:30 +0800

    [mac alias] add notion shortcut page alias

--- mac_alias/notion_alias.sh
+++ mac_alias/notion_alias.sh
@@ -11,3 +11,4 @@ echo "# Notion Alias" >> ~/.bash_profile
 # Notion page
 echo "alias nt_task=\"open -a /Applications/Notion.app https://www.notion.so/antotu/Task-Focus-dc689865e9b54c43bc20f3799e71b11a\"" >> ~/.bash_profile
 echo "alias nt_project=\"open -a /Applications/Notion.app https://www.notion.so/antotu/Project-Focus-843d7cbfb5444ae4bc01b407112e3cb7\"" >> ~/.bash_profile
+echo "alias nt_focus=\"open -a /Applications/Notion.app https://www.notion.so/antotu/Shortcut-Focus-806001319f064642af05fc1a4bea7274\"" >> ~/.bash_profile
