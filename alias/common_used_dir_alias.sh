c418dc7200fc397a61580daed32f4b81ba4f40e5
Dowayching <dowayching@yahoo.com.tw>
2020-05-23 01:09:50 +0800

    [alias] add Project_hq folder alias

--- alias/common_used_dir_alias.sh
+++ alias/common_used_dir_alias.sh
@@ -6,6 +6,7 @@
 
 # Parameters
 PRJ_GIT="~/Project_git"
+PRJ_HQ="~/Documents/Project_hq"
 
 echo "Install Common Used Directory Shortcut..."
 exec 3>&1 1>> ~/.bash_profile
@@ -17,6 +18,7 @@ echo "# Common Used Directory Shortcut"
 # Common Used Folder
 echo "alias dir_git=\"cd ${PRJ_GIT}\""
 echo "alias dir_prjgit=\"cd ${PRJ_GIT}/_project\"" 
+echo "alias dir_prjhq=\"cd ${PRJ_HQ}/\"" 
 echo "alias dir_shortcut=\"cd ${PRJ_GIT}/shortcut_life/alias\"" 
 echo "alias dir_code=\"cd ${PRJ_GIT}/code_chip/\"" 
 echo "alias dir_temp=\"cd ${PRJ_GIT}/shortcut_life/prj_temp\"" 
